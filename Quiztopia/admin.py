935381a9be56c242bff52b222d967661b15f5cd0
Jack Neilson <2881679n@student.gla.ac.uk>
2025-03-06 00:28:20 +0000

    Initial admin and model setup

--- Quiztopia/admin.py
+++ Quiztopia/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from Quiztopia.models import User, Quiz, Question, Answer
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'points', 'quizzes_taken', 'quizzes_created')
+
+class QuizAdmin(admin.ModelAdmin):
+
+    list_display = ('quiz_ID', 'quiz_title', 'category', 'difficulty','upvotes','creator')
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    list_display = ('question_ID','question_text','quiz_ID')
+
+class AnswerAdmin(admin.ModelAdmin):
+
+    list_display = ('answer_ID','answer_text','is_correct','question_ID')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+
--- Quiztopia/models.py
+++ Quiztopia/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    username = models.CharField(max_length=100, unique=True, primary_key=True)
+    profile_picture = models.ImageField(upload_to='',blank=True)
+    points = models.IntegerField(default=0)
+    quizzes_taken = models.IntegerField(default=0)
+    quizzes_created = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+    
+class Quiz(models.Model):
+
+    categories = [
+        ("Movies And TV" , "Movies And TV"),
+        ("Animals" , "Animals"),
+        ("General Knowledge" , "General Knowledge"),
+        ("Sports" , "Sports"),
+        ("Other" , "Other"),
+    ]
+
+    difficulties = [
+        ("Easy" , "Easy"),
+        ("Medium" , "Medium"),
+        ("Hard" , "Hard"),
+    ]
+
+    quiz_ID = models.CharField(max_length=10, unique=True, primary_key=True)
+    quiz_title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100,choices=categories)
+    difficulty = models.CharField(max_length=100,choices=difficulties)
+    upvotes = models.IntegerField(default=0)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return self.quiz_title
+    
+
+class Question(models.Model):
+
+    question_ID = models.CharField(max_length=10, unique=True, primary_key=True)
+    question_text = models.CharField(max_length=500)
+    quiz_ID = models.ForeignKey(Quiz, on_delete=models.CASCADE, to_field='quiz_ID')
+
+    def __str__(self):
+        return self.question_text
+    
+class Answer(models.Model):
+
+    answer_ID = models.CharField(max_length=10, unique=True, primary_key=True)
+    answer_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+    question_ID = models.ForeignKey(Question, on_delete=models.CASCADE, to_field='question_ID')
+
+
+
+    def __str__(self):
+        return self.answer_text
+    
+
+
