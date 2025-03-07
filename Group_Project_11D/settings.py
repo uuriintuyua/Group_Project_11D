46cfe6da46125a119e2deb098014239f4968153a
Jack Neilson <2881679n@student.gla.ac.uk>
2025-03-07 14:39:20 +0000

    Added template directory

--- Group_Project_11D/settings.py
+++ Group_Project_11D/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Group_Project_11D.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
